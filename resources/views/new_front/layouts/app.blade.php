f4c3e5a22534c21448c2dc8df5c21aebb1ab055c
Fadhiel Ibnu Sina <fadhielsina@gmail.com>
2025-01-14 15:19:54 +0700

    meta content

--- resources/views/new_front/layouts/app.blade.php
+++ resources/views/new_front/layouts/app.blade.php
@@ -7,8 +7,8 @@
     <meta charset="utf-8" />
     <title>Indo Teknik Jaya Service Kompor</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport" />
-    <meta content="" name="keywords" />
-    <meta content="" name="description" />
+    <meta content="modena, ariston, legermania, tecnogas" name="keywords" />
+    <meta content="Service Kompor, Jasa Service Kompor" name="description" />
 
     <!-- Favicon -->
     <link href="{{asset('front')}}/img/favicon.ico" rel="icon" />
