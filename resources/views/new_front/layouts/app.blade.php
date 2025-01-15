aac831be1179b568d97d965a5d47b05215cd5c17
Fadhiel Ibnu Sina <fadhielsina@gmail.com>
2025-01-15 10:26:59 +0700

    google ads

--- resources/views/new_front/layouts/app.blade.php
+++ resources/views/new_front/layouts/app.blade.php
@@ -37,6 +37,20 @@
 
     <!-- Template Stylesheet -->
     <link href="{{asset('front')}}/css/style.css" rel="stylesheet" />
+
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=AW-11545691095">
+    </script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'AW-11545691095');
+    </script>
 </head>
 
 <body>
